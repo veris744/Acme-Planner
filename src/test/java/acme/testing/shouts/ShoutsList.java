46ab45f15496aab112ddc37845b4c34f1900d337
Ivan Romero Costa <ivaromcos@alum.us.es>
2021-05-18 20:06:44 +0200

    ShoutsListTest

---
+++ src/test/java/acme/testing/shouts/ShoutsList.java
@@ -0,0 +1,34 @@
+
+package acme.testing.shouts;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.core.annotation.Order;
+
+import acme.testing.AcmePlannerTest;
+
+public class ShoutsList extends AcmePlannerTest {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/anonymous/shouts/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void list(final int recordIndex, final String moment,
+	final String author, final String text, final String info) {
+
+		super.clickOnMenu("Anonymous", "List recent shouts");
+
+		super.checkColumnHasValue(recordIndex, 0, moment);
+		super.checkColumnHasValue(recordIndex, 1, author);
+		super.checkColumnHasValue(recordIndex, 2, text);
+		super.checkColumnHasValue(recordIndex, 3, info);
+
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkInputBoxHasValue("moment", moment);
+		super.checkInputBoxHasValue("author", author);
+		super.checkInputBoxHasValue("text", text);
+		super.checkInputBoxHasValue("info", info);
+
+	}
+
+}

18bf36d5a90c9e1abe466e3634324a517b57a691
Isabel <isadede@alum.us.es>
2021-05-21 11:57:21 +0200

    Task 048

--- src/test/java/acme/testing/administrator/parameters/AdministratorParametersShowTest.java
+++ src/test/java/acme/testing/administrator/parameters/AdministratorParametersShowTest.java
@@ -6,22 +6,19 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.AcmePlannerTest;
 
-public class AdministratorParametersUpdateTest extends AcmePlannerTest {
+public class AdministratorParametersShowTest extends AcmePlannerTest {
+	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/administrator/parameters/update-positive.csv", encoding="utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/administrator/parameters/show.csv", encoding="utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void updatePositive(final String threshold) {
+	public void show(final String threshold) {
 		
 		super.signIn("administrator", "administrator");
-		super.clickOnMenu("Administrator", "See customisation parameters");
-		
-		super.clickOnReturnButton("Update parameters");
-		super.fillInputBoxIn("threshold", threshold);
-		super.clickOnSubmitButton("Update");
 		
 		super.clickOnMenu("Administrator", "See customisation parameters");
 		super.checkInputBoxHasValue("threshold", threshold);
 		
+
 		super.signOut();
 	}
 }

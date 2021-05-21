6246b6a46c0868e0a35579205d555ac9868c61fd
Isabel <isadede@alum.us.es>
2021-05-21 12:27:24 +0200

    Task 050

---
+++ src/test/java/acme/testing/administrator/spamWord/AdministratorSpamWordListTest.java
@@ -0,0 +1,25 @@
+package acme.testing.administrator.spamWord;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.AcmePlannerTest;
+
+public class AdministratorSpamWordListTest extends AcmePlannerTest {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/administrator/spamWord/list.csv", encoding="utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void list(final int recordIndex, final String word) {
+		
+		super.signIn("administrator", "administrator");
+		
+		super.clickOnMenu("Administrator", "See spam words");
+		super.checkColumnHasValue(recordIndex, 0, word);
+		
+		super.signOut();
+		
+	}
+
+}

00d2d53524760e669fd1c7344ac505bce2ef8320
fergongar1 <73081243+fergongar1@users.noreply.github.com>
2021-05-25 20:23:16 +0200

    Update AuthenticatedManagerCreateTest.java

--- src/test/java/acme/testing/authenticated/manager/AuthenticatedManagerCreateTest.java
+++ src/test/java/acme/testing/authenticated/manager/AuthenticatedManagerCreateTest.java
@@ -34,7 +34,7 @@ public class AuthenticatedManagerCreateTest extends AcmePlannerTest{
 	}
 	
 	// En este test comprobamos que se produce un error al intentar registrar un manager en los siguientes casos:
-	// El campo "company" esta vacio, el campo "sector" esta vacio, o ambos campos están vacios.
+	// El campo "company" esta vacio, el campo "sector" esta vacio, o ambos campos estan vacios.
 	@ParameterizedTest
 	@CsvFileSource(resources = "/authenticated/manager/create-negative.csv", encoding="utf-8", numLinesToSkip = 1)
 	@Order(20)

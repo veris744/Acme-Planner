2a393bef0154cf043d08297ce9bae2d826d88e6f
Fernando González <fergongar1@alum.us.es>
2021-06-07 12:55:20 +0200

    Arreglada sugerencia

--- src/main/java/acme/features/manager/workPlan/ManagerWorkPlanShowService.java
+++ src/main/java/acme/features/manager/workPlan/ManagerWorkPlanShowService.java
@@ -1,6 +1,11 @@
 package acme.features.manager.workPlan;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,6 +61,34 @@ public class ManagerWorkPlanShowService implements AbstractShowService<Manager, 
 		
 		model.setAttribute("taskList", tasks);
 		model.setAttribute("enabledTask", enabledTask);
+		
+		//Recomendación fechas
+		if(!tasks.isEmpty()) {
+			final Optional<Date> ini = tasks.stream().min(Comparator.comparing(Task::getStartPeriod)).map(Task::getStartPeriod);
+			final Optional<Date> end = tasks.stream().max(Comparator.comparing(Task::getEndPeriod)).map(Task::getEndPeriod);
+						
+			System.out.println("AAA");
+					
+			if(ini.isPresent() && end.isPresent()) {
+				final LocalDateTime inicio = ini.get().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+				final LocalDateTime fin = end.get().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+									
+				System.out.println(inicio);
+				System.out.println(fin);
+							
+				LocalDateTime inicioRecomendado = inicio.minusDays(1);
+				inicioRecomendado = LocalDateTime.of(inicioRecomendado.getYear(), inicioRecomendado.getMonth(), inicioRecomendado.getDayOfMonth(), 8, 0);
+				
+				LocalDateTime finRecomendado = fin.plusDays(1);
+				finRecomendado = LocalDateTime.of(finRecomendado.getYear(), finRecomendado.getMonth(), finRecomendado.getDayOfMonth(), 17, 0);
+										
+				final Date iniRec = Date.from(inicioRecomendado.atZone(ZoneId.systemDefault()).toInstant());
+				final Date finRec = Date.from(finRecomendado.atZone(ZoneId.systemDefault()).toInstant());
+										
+				model.setAttribute("inirec", iniRec);
+				model.setAttribute("finrec", finRec);
+			}
+		}
 	}
 
 	@Override
